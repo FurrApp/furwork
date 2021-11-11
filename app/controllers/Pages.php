6d52418ec0e0848115bf88f3f37e64364e6ef93b
Ulf FrostyPaw <79121980+ulf-frostypaw@users.noreply.github.com>
2021-11-11 10:12:09 -0600

    Delete app directory

--- app/controllers/Pages.php
+++
@@ -1,8 +0,0 @@
-<?php
-class Pages extends AppController{
-	public function __construct(){}
-
-	public function index(){
-		$this->view('pages/index');
-	}
-}
--- app/helpers/Default.php
+++
@@ -1,8 +0,0 @@
-<?php
-/*
-	* Default helpers
-	* Can be Add Others
-*/
-function to($url = ['']){
-	header("Location: " . APP_URL . "/" . $url . "/");
-}
